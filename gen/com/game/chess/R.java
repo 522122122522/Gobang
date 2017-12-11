29caa0dc7295b50fff5c01ae82a34aa3d6548ed4
Loujianyong <15381147827@163.com>
2017-12-11 10:02:20 +0800

    添加轮盘选择效果

--- gen/com/game/chess/R.java
+++ gen/com/game/chess/R.java
@@ -9,6 +9,51 @@ package com.game.chess;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int circleBackground=0x7f010000;
+        /** <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>East</code></td><td>0</td><td></td></tr>
+<tr><td><code>South</code></td><td>90</td><td></td></tr>
+<tr><td><code>West</code></td><td>180</td><td></td></tr>
+<tr><td><code>North</code></td><td>270</td><td></td></tr>
+</table>
+         */
+        public static final int firstChildPosition=0x7f010001;
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int isRotating=0x7f010003;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int name=0x7f010004;
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int rotateToCenter=0x7f010002;
     }
     public static final class color {
         public static final int colorAccent=0x7f040002;
@@ -29,28 +74,30 @@ public final class R {
         public static final int bottom=0x7f020004;
         public static final int bottom_left=0x7f020005;
         public static final int bottom_right=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int left=0x7f020008;
-        public static final int left_white=0x7f020009;
-        public static final int line=0x7f02000a;
-        public static final int right=0x7f02000b;
-        public static final int right_black=0x7f02000c;
-        public static final int timg=0x7f02000d;
-        public static final int top=0x7f02000e;
-        public static final int top_left=0x7f02000f;
-        public static final int top_right=0x7f020010;
-        public static final int white=0x7f020011;
+        public static final int button_select=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int left=0x7f020009;
+        public static final int left_white=0x7f02000a;
+        public static final int line=0x7f02000b;
+        public static final int right=0x7f02000c;
+        public static final int right_black=0x7f02000d;
+        public static final int timg=0x7f02000e;
+        public static final int top=0x7f02000f;
+        public static final int top_left=0x7f020010;
+        public static final int top_right=0x7f020011;
+        public static final int white=0x7f020012;
     }
     public static final class id {
-        public static final int boardImageView=0x7f070007;
-        public static final int button_regret_undo=0x7f070006;
-        public static final int button_repeat_display=0x7f070005;
-        public static final int button_restart=0x7f070004;
-        public static final int checkboxAI=0x7f070001;
-        public static final int chessImageView=0x7f070008;
-        public static final int gridview=0x7f070003;
-        public static final int relativelayout_title=0x7f070000;
-        public static final int textview_notify=0x7f070002;
+        public static final int East=0x7f070000;
+        public static final int North=0x7f070001;
+        public static final int South=0x7f070002;
+        public static final int West=0x7f070003;
+        public static final int boardImageView=0x7f070008;
+        public static final int checkboxAI=0x7f070005;
+        public static final int chessImageView=0x7f070009;
+        public static final int gridview=0x7f070007;
+        public static final int relativelayout_title=0x7f070004;
+        public static final int textview_notify=0x7f070006;
     }
     public static final class layout {
         public static final int activity_gomoku=0x7f030000;
@@ -59,4 +106,113 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f060000;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a Circle.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #Circle_circleBackground com.game.chess:circleBackground}</code></td><td></td></tr>
+           <tr><td><code>{@link #Circle_firstChildPosition com.game.chess:firstChildPosition}</code></td><td></td></tr>
+           <tr><td><code>{@link #Circle_isRotating com.game.chess:isRotating}</code></td><td></td></tr>
+           <tr><td><code>{@link #Circle_rotateToCenter com.game.chess:rotateToCenter}</code></td><td></td></tr>
+           </table>
+           @see #Circle_circleBackground
+           @see #Circle_firstChildPosition
+           @see #Circle_isRotating
+           @see #Circle_rotateToCenter
+         */
+        public static final int[] Circle = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.game.chess.R.attr#circleBackground}
+          attribute's value can be found in the {@link #Circle} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.game.chess:circleBackground
+        */
+        public static final int Circle_circleBackground = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.game.chess.R.attr#firstChildPosition}
+          attribute's value can be found in the {@link #Circle} array.
+
+
+          <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>East</code></td><td>0</td><td></td></tr>
+<tr><td><code>South</code></td><td>90</td><td></td></tr>
+<tr><td><code>West</code></td><td>180</td><td></td></tr>
+<tr><td><code>North</code></td><td>270</td><td></td></tr>
+</table>
+          @attr name com.game.chess:firstChildPosition
+        */
+        public static final int Circle_firstChildPosition = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.game.chess.R.attr#isRotating}
+          attribute's value can be found in the {@link #Circle} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.game.chess:isRotating
+        */
+        public static final int Circle_isRotating = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.game.chess.R.attr#rotateToCenter}
+          attribute's value can be found in the {@link #Circle} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.game.chess:rotateToCenter
+        */
+        public static final int Circle_rotateToCenter = 2;
+        /** Attributes that can be used with a CircleImageView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CircleImageView_name com.game.chess:name}</code></td><td></td></tr>
+           </table>
+           @see #CircleImageView_name
+         */
+        public static final int[] CircleImageView = {
+            0x7f010004
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.game.chess.R.attr#name}
+          attribute's value can be found in the {@link #CircleImageView} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.game.chess:name
+        */
+        public static final int CircleImageView_name = 0;
+    };
 }
